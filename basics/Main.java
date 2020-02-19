3452104b1b8e8c3f0bb3b108ef8075a24089a94f
Adrienne <adrienneeaston@gmail.com>
2020-02-18 17:30:14 -0800

    conditionals

--- basics/Main.java
+++ basics/Main.java
@@ -23,6 +23,26 @@ public class Main {
   }
 }
 
+//Conditionals
+//Change the variables in the first section, so that each if statement resolves as true.
+
+public class Main {
+  public static void main(String[] args) {
+      String a = new String("Wow");
+      String c = "Wow!";
+      String b = a;
+      String d = c;
+
+      boolean b1 = a == b;
+      boolean b2 = d.equals(b + "!");
+      boolean b3 = !c.equals(a);
+
+      if (b1 && b2 && b3) {
+          System.out.println("Success!");
+      }
+  }
+}
+
 //Functions
 //Write the method printFullName of student which prints the full name of a student.
 
